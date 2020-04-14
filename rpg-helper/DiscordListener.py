4343a0b6fa9d7c0a358e27869ec964ac11163933
Jonathan <jbborre@gmail.com>
2020-04-13 22:24:25 -0500

    updated for listening to messages

--- rpg-helper/DiscordListener.py
+++ rpg-helper/DiscordListener.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+from builtins import print
 
 import discord
 from dotenv import load_dotenv
@@ -12,5 +13,16 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        print(message)
+        await message.channel.send(msg)
 
 client.run(TOKEN)
